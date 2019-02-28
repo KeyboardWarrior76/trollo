b85afbe32e971e5f82a694b0f02e535626398f95
3mpirical <3mpirical10510@gmail.com>
2019-02-28 15:07:38 -0700

    created inital routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show]
+
+  resources :boards do
+    resources :list, only: [:create, :update, :destroy]
+  end
+
+  post "lists/:id/items" => "item#create"
+  put "lists/:id/items/:id" => "item#update"
+  delete "lists/:id/items/:id" => "item#destroy"
+
+
 end
