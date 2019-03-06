1a75477f63c2aae939b212dfeeb6684b216aff9e
3mpirical <3mpirical10510@gmail.com>
2019-03-05 22:51:55 -0700

    added logo to navbar on all pages except board-show, and user-controller no longer passes @boards on user-show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,6 @@ class UsersController < ApplicationController
   def show
     if(params[:id].to_i != current_user.id)
       @user = User.get_user(params[:id])
-      @boards = Board
-                .get_user_boards(params[:id])
-                .select() {|item|
-                  item.public == true
-                }
     end
   end
 
